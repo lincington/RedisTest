02e66a6e1a24d10c4b6def803099b5b37fc62055
lincington <1226645965@qq.com>
2019-03-04 19:24:51 +0800

    提交文件

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+     public  class Student
+    {
+        string id;
+        string name;
+        string age;
+        string sex;
+        string address;
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Age { get => age; set => age = value; }
+        public string Sex { get => sex; set => sex = value; }
+        public string Address { get => address; set => address = value; }
+    }
+}
